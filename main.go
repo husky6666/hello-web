4047311892a4386c7a72f5e3ae9250cc53fd6264
husky <husky.robot.dog@gmail.com>
2022-02-15 17:28:53 +0800

    init

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"context"
+	"google.golang.org/grpc"
+	"hello-web/pb_service"
+	"log"
+)
 
+func main() {
+	conn, err := grpc.Dial("127.0.0.1:9000", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	service := pb_service.NewHelloServiceClient(conn)
+	response, err := service.SayHello(context.Background(), &pb_service.HelloRequest{Name: "husky"})
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	log.Println(response.GetMessage())
+	defer conn.Close()
 }
